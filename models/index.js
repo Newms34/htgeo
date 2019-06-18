0e340d37d81147b55675e7193de136756d759fa9
newms34 <newms3450@gmail.com>
2019-06-17 23:11:55 -0400

    yello

--- models/index.js
+++ models/index.js
@@ -7,9 +7,9 @@ require('./blog/');
 console.log('Node Environment:', process.env.NODE_ENV)
 if (!process.env.NODE_ENV || process.env.NODE_ENV != 'prod') {
     //just some quick env check. If we're developing locally, go ahead and use our local db. Otherwise, use the mlab db.
-    mongoose.connect('mongodb://localhost:27017/htgeo');
+    mongoose.connect('mongodb://localhost:27017/htgeo',{useNewUrlParser:true});
 } else {
-    mongoose.connect(process.env.MONGODB_URI);
+    mongoose.connect(process.env.MONGODB_URI,{useNewUrlParser:true});
 }
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
